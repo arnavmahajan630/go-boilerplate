ba6317bfe1c3d50513dde9682a1448e740c22cd1
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-01-03 12:14:02 +0530

    feat/test-task-run-successful

--- apps/backend/cmd/go-boilerplate/main.go
+++ apps/backend/cmd/go-boilerplate/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func welcome(w http.ResponseWriter, r * http.Request) {
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello to this welcome endpoint!"))
+}
 
 func main() {
 
-	fmt.Println("Staring Point :)")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/welcome", welcome)
+	fmt.Println("Server started on Port 8080 :)")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Error starting the server")
+	}
 }
